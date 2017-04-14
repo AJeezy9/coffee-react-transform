1d648e2b0e3ccb384a2e94f91c9287f73624c418
Steve Fan <cia48621793@gmail.com>
2017-04-14 12:20:05 +0800

    update

--- src/stringescape.js
+++ src/stringescape.js
@@ -1,39 +1,35 @@
+module.exports = function stringEncode (input, opts) {
+  opts = opts || {}
+  var escaped = ''
 
-var hex = '0123456789abcdef'.split('');
-
-module.exports  =  function stringEncode(input, opts) {
-  opts = opts || {};
-  var escaped = "";
-  
   for (var i = 0; i < input.length; i++) {
-    escaped = escaped + encodeChar(input.charAt(i), opts.preserveNewlines);
+    escaped = escaped + encodeChar(input.charAt(i), opts.preserveNewlines)
   }
-  
-  return escaped;
+
+  return escaped
 }
 
-function encodeChar(inputChar, preserveNewlines) {
-  var character = inputChar.charAt(0);
-  var characterCode = inputChar.charCodeAt(0);
+function encodeChar (inputChar, preserveNewlines) {
+  var character = inputChar.charAt(0)
 
-  switch(character) {
+  switch (character) {
     case '\n':
-      if (!preserveNewlines) return "\\n";
-      else return character;
+      if (!preserveNewlines) return '\\n'
+      else return character
     case '\r':
-      if (!preserveNewlines) return "\\r";
-      else return character;
-    case '\'': return "\\'";
-    case '"': return "\\\"";
-    case '\&': return "\\&";
-    case '\\': return "\\\\";
-    case '\t': return "\\t";
-    case '\b': return "\\b";
-    case '\f': return "\\f";
-    case '/': return "\\x2F";
-    case '<': return "\\x3C";
-    case '>': return "\\x3E";
+      if (!preserveNewlines) return '\\r'
+      else return character
+    case '\'': return "\\'"
+    case '"': return '\\"'
+    case '&': return '\\&'
+    case '\\': return '\\\\'
+    case '\t': return '\\t'
+    case '\b': return '\\b'
+    case '\f': return '\\f'
+    case '/': return '\\x2F'
+    case '<': return '\\x3C'
+    case '>': return '\\x3E'
   }
 
-  return inputChar;
+  return inputChar
 }
